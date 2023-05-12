06f43f6385afbda6cfa586691d32e6f3dcd02f54
Tobi OLADELE <oladeletobiadedoyin@gmail.com>
2023-05-12 08:02:04 +0100

    updating the game logic

--- script.js
+++ script.js
@@ -11,15 +11,6 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const diceEl = document.querySelector('.dice');
 
-score0EL.textContent = 0;
-score1EL.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-
 const switchPlayer = () => {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -27,6 +18,27 @@ const switchPlayer = () => {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active ');
 };
+let scores, currentScore, activePlayer, playing;
+
+const init = () => {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 btnRoll.addEventListener('click', () => {
   if (playing) {
@@ -53,6 +65,7 @@ btnHold.addEventListener('click', () => {
 
     if (scores[activePlayer] >= 100) {
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
@@ -64,4 +77,8 @@ btnHold.addEventListener('click', () => {
       switchPlayer();
     }
   }
+});
+
+btnNew.addEventListener('click', () => {
+  init();
 });
